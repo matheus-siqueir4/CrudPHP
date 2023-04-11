05baa71747a947e54fb746267e4855235666ee11
matheus-siqueir4 <lima900100@gmail.com>
2023-04-11 08:39:27 -0300

    conexão segura

--- classes/Pessoa.php
+++ classes/Pessoa.php
@@ -1,52 +1,72 @@
 <?php
 
 class Pessoa {
+    
+    private static $conn;
+
+    public static function getConnection() {
+        
+        if (empty(self::$conn)) { 
+            $conexao = parse_ini_file('../config/livro.ini');
+            $host = $conexao['host'];
+            $db = $conexao['db'];
+            $user = $conexao['user'];
+            $pass = $conexao['pass'];
+
+            self::$conn = new PDO("mysql:host={$host};dbname={$db}, {$user}, {$pass}");
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$conn;
+    }
 
     public static function save($pessoa) {
-        $conn = new PDO('mysql:host=localhost;dbname=livro', 'root', '');
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+        
+        $conn = self::getConnection();
+
         if (empty($pessoa['id'])) {
             $result = $conn->query("SELECT max(id) as next FROM pessoa");
             $row = $result->fetch();
             $pessoa['id'] = (int) $row['next'] + 1;
-
-            $sql = "INSERT INTO pessoa (id, nome, endereco, bairro, 
-            telefone, email, id_cidade) VALUES ('{$pessoa['id']}', 
-            '{$pessoa['nome']}', '{$pessoa['endereco']}', 
-            '{$pessoa['bairro']}', '{$pessoa['telefone']}', 
-            '{$pessoa['email']}', '{$pessoa['id_cidade']}')";
+            
+            $sql = "INSERT INTO pessoa (id, nome, endereco, bairro, telefone, email, id_cidade)
+            VALUES (:id, :nome, :endereco, :bairro, :telefone, :email, :id_cidade)";
         }
         else {
-            $sql = "UPDATE pessoa SET nome = '{$pessoa['nome']}', 
-            endereco = '{$pessoa['endereco']}', bairro = '{$pessoa['bairro']}',
-            telefone = '{$pessoa['telefone']}', email = '{$pessoa['email']}', 
-            id_cidade = '{$pessoa['id_cidade']}' WHERE id = '{$pessoa['id']}'";
+            $sql = "UPDATE pessoa SET nome = :nome, endereco = :endereco,
+            bairro = :bairro, telefone = :telefone, email = :email,
+            email = :email, id_cidade = :id_cidade";
         }
 
-        $conn->query($sql);
+        $conn->prepare($sql);
+        $conn->execute([':id' =>  $pessoa['id'],
+        ':nome' => $pessoa['nome'],
+        ':endereco' => $pessoa['endereco'],
+        ':bairro' => $pessoa['bairro'],
+        ':telefone' => $pessoa['telefone'],
+        ':email' => $pessoa['email'],
+        ':id_cidade' => $pessoa['id_cidade']]); 
     }
 
     public static function find($id) {
-        $conn = new PDO('mysql:host=localhost;dbname=livro', 'root', '');
+        $conn = self::getConnection();
         $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $result = $conn -> query("SELECT * FROM pessoa WHERE id = '{$id}'");
-        return $result -> fetch();
+        $result = $conn->query("SELECT * FROM pessoa WHERE id = '{$id}'");
+        return $result->fetch();
     }
 
     public static function delete($id) {
-        $conn = new PDO('mysql:host=localhost;dbname=livro', 'root', '');
+        $conn = self::getConnection();
         $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $result = $conn -> query("DELETE FROM pessoa WHERE id = '{$id}'");
-        return $result -> fetch();
+        $result = $conn->query("DELETE FROM pessoa WHERE id = '{$id}'");
+        return $result->fetch();
     }
 
     public static function all() {
         $conn = new PDO('mysql:host=localhost;dbname=livro', 'root', '');
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $result = $conn -> query("SELECT * FROM pessoa ORDER BY id");
-        return $result -> fetchAll();
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $result = $conn->query("SELECT * FROM pessoa ORDER BY id");
+        return $result->fetchAll();
 
     }
 }
--- form/listaPessoa.php
+++ form/listaPessoa.php
@@ -1,5 +1,5 @@
 <?php
-  require_once 'pessoaDatabase.php';
+
   require_once '../classes/Pessoa.php';
   if (!empty($_GET['action']) and $_GET['action'] == 'delete') {
   $id = (int) $_GET['id'];
