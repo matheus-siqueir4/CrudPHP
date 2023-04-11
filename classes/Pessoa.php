7beb552d0b58e1e6802dae7f2fba2c67dd8c2be7
matheus-siqueir4 <lima900100@gmail.com>
2023-04-11 17:32:59 -0300

    PDO

--- classes/Pessoa.php
+++ classes/Pessoa.php
@@ -4,18 +4,18 @@ class Pessoa {
     
     private static $conn;
 
-    public static function getConnection() {
-        
+    public static function getConnection() {        
         if (empty(self::$conn)) { 
             $conexao = parse_ini_file('../config/livro.ini');
             $host = $conexao['host'];
             $db = $conexao['db'];
             $user = $conexao['user'];
             $pass = $conexao['pass'];
-
-            self::$conn = new PDO("mysql:host={$host};dbname={$db}, {$user}, {$pass}");
+            
+            self::$conn = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+
         return self::$conn;
     }
 
@@ -34,17 +34,23 @@ class Pessoa {
         else {
             $sql = "UPDATE pessoa SET nome = :nome, endereco = :endereco,
             bairro = :bairro, telefone = :telefone, email = :email,
-            email = :email, id_cidade = :id_cidade";
+            id_cidade = :id_cidade 
+            WHERE id = :id";
         }
 
-        $conn->prepare($sql);
-        $conn->execute([':id' =>  $pessoa['id'],
-        ':nome' => $pessoa['nome'],
-        ':endereco' => $pessoa['endereco'],
-        ':bairro' => $pessoa['bairro'],
-        ':telefone' => $pessoa['telefone'],
-        ':email' => $pessoa['email'],
-        ':id_cidade' => $pessoa['id_cidade']]); 
+        $conn
+            ->prepare($sql)
+            ->execute([
+                ':id'        => $pessoa['id'],
+                ':nome'      => $pessoa['nome'],
+                ':endereco'  => $pessoa['endereco'],
+                ':bairro'    => $pessoa['bairro'],
+                ':telefone'  => $pessoa['telefone'],
+                ':email'     => $pessoa['email'],
+                ':id_cidade' => $pessoa['id_cidade']
+            ]);
+            
+        return true;
     }
 
     public static function find($id) {
@@ -63,8 +69,9 @@ class Pessoa {
     }
 
     public static function all() {
-        $conn = new PDO('mysql:host=localhost;dbname=livro', 'root', '');
+        $conn = new PDO('mysql:host=localhost;dbname=livro', 'lima', 'lima123');
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         $result = $conn->query("SELECT * FROM pessoa ORDER BY id");
         return $result->fetchAll();
 
